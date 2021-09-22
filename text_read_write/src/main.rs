113228333cd10bb3825f707b0be11465b02d9738
Ryo Raizen <ryo-raizen@outlook.jp>
2021-09-22 02:01:57 -0400

    fix text_read_write (append)

--- text_read_write/src/main.rs
+++ text_read_write/src/main.rs
@@ -1,20 +1,34 @@
 extern crate path_absolutize;
-use std::path::Path;
-use std::fs;
+use std::fs::OpenOptions;
 use std::io;
+use std::io::Read;
+use std::io::Write;
+use std::path::Path;
 use path_absolutize::*;
 
 fn main() {
 	let filename = Path::new("./data/sample.txt").absolutize().unwrap();
-	let contents = fs::read_to_string(filename.clone())
+
+	let mut file = OpenOptions::new()
+		.create(true)
+		.write(true)
+		.read(true)
+		.append(true)
+		.open(filename)
+		.expect("Something went wrong opening the file");
+
+	let mut contents = String::new();
+	file.read_to_string(&mut contents)
 		.expect("Something went wrong reading the file");
 
 	println!("### File contents ###");
 	println!("{}", contents);
+
 	println!("### Please input text.");
 	let mut input = String::new();
 	io::stdin()
 		.read_line(&mut input)
 		.expect("Failed to read line");
-	fs::write(filename.clone(), input).expect("Unable to write file");
+	file.write_all(input.as_bytes())
+		.expect("Something went wrong writing the file");
 }
